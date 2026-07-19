e22f9fe3fdfc9070ea9fe261238bc1d62aa54ac6
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-07-19 12:42:44 +0530

    Add navbar and logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const { user, token } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+
+    navigate("/");
+  };
+
   return (
-    <nav className="bg-blue-600 text-white p-4 shadow">
-      <h1 className="text-xl font-bold">Task Manager</h1>
+    <nav className="bg-blue-600 text-white px-6 py-4 shadow">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">MERN Task Manager</h1>
+
+        {token && user ? (
+          <div className="flex items-center gap-4">
+            <span>Hello, {user.name}</span>
+
+            <button
+              onClick={handleLogout}
+              className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-100"
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </nav>
   );
 };
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 const Dashboard = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">Dashboard</h1>
     </div>
   );
