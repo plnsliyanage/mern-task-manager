8f7439606f92f9fccdc9941fe0dc5b298247c6bc
plnsliyanage <liyanage12854@usci.ruh.ac.lk>
2026-07-20 10:57:41 +0530

    Prepare frontend for deployment

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,27 +2,36 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+
 });
 
 
 // Add JWT token automatically
 API.interceptors.request.use(
+
   (config)=>{
 
     const token = localStorage.getItem("token");
 
     if(token){
+
       config.headers.Authorization =
       `Bearer ${token}`;
+
     }
 
     return config;
+
   },
 
   (error)=>{
+
     return Promise.reject(error);
+
   }
+
 );
 
 
